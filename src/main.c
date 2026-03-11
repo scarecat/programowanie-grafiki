677c6d90fe93c1b7a7dbf171cf857503f9067ad4
scarecat <scarecat@tuta.io>
2026-03-11 15:10:42 +0100

    lab0

--- src/main.c
+++ src/main.c
@@ -1,16 +1,38 @@
 #include <GL/freeglut.h> // dodanie biblioteki GLUT
-#include <GL/glu.h>
+#include <GL/freeglut_std.h>
+#include <GL/gl.h>
 #include <stdlib.h>
+
+enum Shapes {
+  SPHERE,
+  TEAPOT,
+  CONE,
+};
+
+
+enum Shapes shape; // co ma narysować
+
 
 // funkcja obsługi klawiatury
 void klawiatura(unsigned char key, int x, int y)
 {
   switch (key)
   {
+    case '1':
+      shape = SPHERE;
+      break;
+    case '2':
+      shape = CONE;
+      break;
+    case '3':
+      shape = TEAPOT;
+      break;
+
+
     case 27:
     case 'q':
-    exit(0);
-    break;
+      exit(0);
+      break;
   }
 }
 
@@ -18,7 +40,9 @@ void reshape(int w, int h) {
   glViewport(0, 0, (GLsizei) w, (GLsizei) h);
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  gluOrtho2D(0.0, (GLdouble) w, 0.0, (GLdouble) h);
+  //gluOrtho2D(0.0, (GLdouble) w, 0.0, (GLdouble) h);
+  //gluOrtho2D(0.0, (GLdouble) w, 0.0, (GLdouble) h);
+  glOrtho(0.0f, (GLdouble) w, 0.0f, (GLdouble)h, -100.0f, 100.0f);
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
 }
@@ -33,6 +57,7 @@ void drawRect(int x, int y, int w, int h) {
 
 // funkcja wyświetlająca obraz
 void display() {
+  glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT);
   glColor3f(0.0, 1.0, 0.0);
   /*
@@ -42,20 +67,36 @@ void display() {
   drawRect(150, 220, 100, 30);
   glEnd();
   */
-
+  glPushMatrix();
   glTranslatef(120.0f, 120.0f, 0.0f);
-  glutSolidTeapot(100);
+
+  switch (shape) {
+    case SPHERE:
+      glutSolidSphere(100, 32, 16);
+      break;
+    case CONE:
+      glRotatef(270.0f, 1.0f, 0.0f, 0.0f);
+      glutSolidCone(100.0f, 100.0f, 32, 16);
+      break;
+    case TEAPOT:
+      glutSolidTeapot(100);
+      break;
+  }
+
+  glPopMatrix();
   glutSwapBuffers();
+  glutPostRedisplay();
 }
 
 int main(int argc, char *argv[]) {
   glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB); // ustawienie podwójnego
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH); // ustawienie podwójnego
   glutInitWindowSize(640, 480); // rozmiar okna
   glutCreateWindow(argv[0]);
   glutDisplayFunc(display); // ustawienie funkcji odpowiedzialnej za
   glutReshapeFunc(reshape); // ustawienie funkcji wywoływanej przy
   glutKeyboardFunc(klawiatura); // ustawienie funkcji obsługi klawiatury
   glutMainLoop(); // wejście do głównej pętli programu
+  glEnable(GL_DEPTH_TEST);
   return 0;
 }
